7d95ee60794d784afce697ac2992b7a2620dc451
rocket3989 <rocket3989@gmail.com>
2019-12-11 18:53:07 -0600

    finished ceasar page

--- script.js
+++ script.js
@@ -5,29 +5,66 @@ var startPointer
 var pointerDown = false
 var seg = 360 / 26
 var key = 0
+var last = 'encrypt'
 
 
 spin = window.setInterval(function(){
     seg = 360 / 26
-    setAngle(offsetAngle)           
-    offsetAngle+= .1
+    offsetAngle += seg 
+    offsetAngle %= 360
+    setAngle(offsetAngle, true, .5)  
     updateKey()
-}, 5);
+}, 500);
 
 function updateKey(){
     key = (26 + Math.floor((offsetAngle + resAngle + .5 * seg) / seg)) % 26
-    $('#key').text(key)
+    $('#key').val(key)
+    textUpdate()
 }
 
 $('#key').on('input', function(){ 
-    newAngle = $(this).text()
-    setAngle(newAngle * seg, true, Math.abs((offsetAngle / seg) - newAngle) * .4)
+    newAngle = $(this).val()
+    setAngle(newAngle * seg, true, Math.abs((offsetAngle / seg) - newAngle) * .3)
     offsetAngle = newAngle * seg
-});
+    textUpdate()
+})
+
+$('#encrypt').on('input', function(){
+    last = 'encrypt'
+    textUpdate()
+})
+
+$('#decrypt').on('input', function(){
+    last = 'decrypt'
+    textUpdate()
+})
+
+
+function textUpdate(){
+    if (last == 'encrypt')
+        text = $('#encrypt').val()
+    else
+        text = $('#decrypt').val()
+
+    text = text.toLowerCase()
+    outText = []
+    var regex = "[.,!?\\- /_\n]"
+    text.split('').forEach(char => {
+        newChar = String.fromCharCode((26 + char.charCodeAt(0) + (last == 'encrypt' ? key:-key) - 'a'.charCodeAt(0)) % 26 + 'a'.charCodeAt(0))
+
+        outText.push(char.match(regex) != null ? char : newChar )
+        
+    });
+    if (last == 'encrypt')
+        $('#decrypt').val(outText.join(''))
+    else
+        $('#encrypt').val(outText.join(''))
+
+}
 
 function setAngle(angle, transition = false, length = 1){
     if (transition)
-        $("#ringOut").css({transition: "transform " +length+ "s", transform:"rotate("+angle+"deg)"}); 
+        $("#ringOut").css({transition: "transform " +length+ "s", transform:"rotate("+angle+"deg)", 'transition-timing-function': "linear"}); 
     else
         $("#ringOut").css({transition: 'none',transform:"rotate("+angle+"deg)"}); 
 }
@@ -46,14 +83,14 @@ $("body").mousemove(function(){
         resAngle = Math.atan2(event.clientY - ringCenter.y, event.clientX - ringCenter.x) - 
         Math.atan2(startPointer.y - ringCenter.y, startPointer.x - ringCenter.x);
         resAngle *= 180 / Math.PI
-        setAngle(offsetAngle + resAngle)
+        setAngle((offsetAngle + resAngle) % 360)
         updateKey()
     }                           
 })
 
 function end(){
     if(pointerDown){
-        offsetAngle = seg * Math.floor((offsetAngle + resAngle + .5 * seg)/seg)
+        offsetAngle = (seg * Math.floor((offsetAngle + resAngle + .5 * seg)/seg))%360
         setAngle(offsetAngle, true) 
         pointerDown = false
         resAngle = 0
@@ -62,4 +99,4 @@ function end(){
 }
 
 $("body").mouseup(function(){end()})
-$("html").mouseleave(function(){end()})
+$("body").mouseleave(function(){end()})
